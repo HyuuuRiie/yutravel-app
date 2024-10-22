1f371949a793609b76d902be726137d892a4a002
Fajri <wahyuseptiadifajri11@gmail.com>
2024-10-22 14:48:12 +0700

    finishing get apps section

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -8,7 +8,7 @@ const Guide = () => {
         <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>We are here just for you</p>
         <div className='flex flex-wrap justify-between gap-5 lg:gap-10'>
           <h2 className='bold-40 lg:bold-64 xl:max-w-[390px]'>Guide You to Easy Path</h2>
-          <p className='regular-16 text-gray-30 xl:max-w-[520px]'>Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
+          <p className='regular-16 text-gray-30 xl:max-w-[520px]'>Only with the Yutravel application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
         </div>
       </div>
       <div className="flexCenter max-container relative w-full">
